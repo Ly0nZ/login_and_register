e3464413c5fc8c00c9d1f86e5079861ccb283c58
Ly0nZ <charleymlyons@gmail.com>
2018-01-26 10:31:28 -0800

    Login form fields and form routing are complete. Conditionals in place to dislay error messages iflogin information is not satisfactory. Password encryption with Bcrypt 'in progress'.

--- apps/login/views.py
+++ apps/login/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render, redirect
+from __future__ import unicode_literals
 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
 # Create your views here.
 def index(request):
 	return render(request, 'login/index.html')
 
 def register(request):
-	print(request.POST)
+	errors = []
+	for key, val in request.POST.items():
+		if len(val) < 3:
+			errors.append("{} must be at least two characters".format(key))
+
+	if request.POST['password'] != request.POST['password_confirmation']:
+		errors.append["Error: Password and password confirmation do not match."]
+
+	if errors:
+		for err in errors:
+			messages.error(request, err)
+
+	else:
+
+		hashpw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+		print(hashpw)
 	return redirect('/')
